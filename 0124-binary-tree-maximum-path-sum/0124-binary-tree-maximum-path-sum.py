6028e90e2e5e1436e1ba8c1cf355fbf8c26ec286
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-19 21:34:00 -0500

    Time: 17 ms (50.72%), Space: 23.2 MB (13.62%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -11,20 +11,20 @@ class Solution:
         def traverse(node):
             nonlocal res
             if not node:
-                print(f"\nat node : {node} ")
+                # print(f"\nat node : {node} ")
                 return -float('inf')
-            print(f"\nat node : {node.val} --------------------------------------------------------------------------")
+            # print(f"\nat node : {node.val} --------------------------------------------------------------------------")
             
 
             l = traverse(node.left)
             r = traverse(node.right)
 
-            print(f"finding res in {res,l, r, l+node.val, r+node.val, l+node.val+r, node.val}")
+            # print(f"finding res in {res,l, r, l+node.val, r+node.val, l+node.val+r, node.val}")
             res = max(res,l, r, l+node.val, r+node.val, l+node.val+r, node.val)
-            print(f"at node: {node.val} leftpath = {l}, right_path= {r}, res : {res}")
+            # print(f"at node: {node.val} leftpath = {l}, right_path= {r}, res : {res}")
 
             if l == r== -float('inf') :  return node.val
-            print(f"going to final return l, r : {l,r}")
+            # print(f"going to final return l, r : {l,r}")
             return max(l+node.val, r+node.val, node.val)
         traverse(root)
         return res
