28272daffa0fd3f6b38ac1ef0c3f4e7f836d8cdb
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-21 17:10:51 -0500

    Time: 7 ms (63.53%), Space: 21.8 MB (49.07%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -6,11 +6,7 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
 
-        def show(board):
-            for row in board:
-                print(row)
-        print(f"initial board : - ")
-        show(board)
+
         m, n = len(board), len(board[0])
 
         visited = [[False for _ in range(n)] for __ in range(m)]
@@ -25,25 +21,18 @@ class Solution:
                     board[i][j] = 1
                     reverse.append((i,j))
 
-        print(f"after marking boundayr Os --- ")
-        show(board)
-        print(f"\nQ : {Q}")
         dirs = [(0,1), (0,-1), (1,0), (-1,0)]
         
         while Q:
             i,j = Q.popleft()
-            print(f"\n reached : {i,j} - ---------------------------------------")
             for dx, dy in dirs:
                 x,y = i+dx, j+dy
-                print(f"exploring : {x,y} ------------------")
                 if 0<=x<m and 0<=y<n and board[x][y]=="O" and not visited[x][y]:
-                    print(f"marking : {x,y}")
                     board[x][y] = 1
                     reverse.append((x,y))
                     Q.append((x,y))
                     visited[x][y] = True
-        print(f"\nafter bfs ---")
-        show(board)
+
         for i in range(1,m):
             for j in range(1,n):
                 if board[i][j]=="O": board[i][j] = "X"
