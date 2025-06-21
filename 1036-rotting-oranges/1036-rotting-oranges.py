db136f627ea4a79d56e7b87d054a7d7b15a42f32
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-21 13:46:13 -0500

    Time: 3 ms (82.06%), Space: 17.8 MB (71.08%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -4,14 +4,11 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
 
-        # visited = [[0 for _ in range(cols)] for __ in range(rows)]
-
-
         Q = deque()
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j]==2:
-                    # visited[i][j]==1
+                    
                     Q.append((i,j,0))
 
         dr = [(0,1), (1,0), (0,-1), (-1,0)]
