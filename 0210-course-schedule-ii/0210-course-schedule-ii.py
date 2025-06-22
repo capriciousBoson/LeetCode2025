e1dbd6fd467fa7a0e5e2b0d7fae838217d308cd1
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-22 17:39:10 -0500

    Time: 0 ms (100%), Space: 19.1 MB (72.03%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,28 @@
+from collections import defaultdict, deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        visited = [False for _ in range(numCourses)]
+        indegrees = [0 for _ in range(numCourses)]
+        adj = defaultdict(list)
+        res = []
+        for a,b in prerequisites:
+            adj[b].append(a)
+            indegrees[a] +=1
+        
+        Q = deque()
+        for i in range(numCourses):
+            if indegrees[i] == 0:
+                Q.append(i)
+        while Q:
+            current = Q.popleft()
+            res.append(current)
+            for node in adj[current]:
+                indegrees[node] -= 1
+                if indegrees[node] == 0:
+                    Q.append(node)
+
+        if len(res)==numCourses:
+            return res
+
+        return []
+        
