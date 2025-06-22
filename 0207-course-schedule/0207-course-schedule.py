986d1787a5fdca232f41644f8bfae9379a38a7aa
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-22 16:22:56 -0500

    Time: 4 ms (67.7%), Space: 19.5 MB (21.81%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visited = [False for _ in range(numCourses)]
+        path_visited = [False for _ in range(numCourses)]
+
+        adj = defaultdict(list)
+        for a,b in prerequisites:
+            adj[b].append(a)
+
+
+        def dfs(node):
+            for neighbor in adj[node]:
+                if not visited[neighbor]:
+                    visited[neighbor] = True
+                    path_visited[neighbor] = True
+                    if dfs(neighbor): return True
+                    path_visited[neighbor] = False
+                elif visited[neighbor] and path_visited[neighbor]:
+                    return True
+            return False
+                    
+
+        for start in range(numCourses):
+            if not visited[start]:
+                if dfs(start):
+                    return False
+        return True
+        
+        
