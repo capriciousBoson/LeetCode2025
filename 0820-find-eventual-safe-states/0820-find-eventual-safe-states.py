716a9fa614c5ccce4bc3edced7e8eda35b641e44
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-22 21:00:46 -0500

    Time: 71 ms (24.39%), Space: 23.7 MB (51.26%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -1,44 +1,34 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         
         n = len(graph)
         adj = defaultdict(list)
-        safenodes = [0 for i in range(n)]
-       
-        visited = [False for _ in range(n)]
-        path_visited= [False for __ in range(n)]
+        indegrees = [0 for _ in range(n)]
 
+        # reverse graph adjacency list
         for i in range(n):
             for node in graph[i]:
-                adj[i].append(node)
-            
-    
-        def dfs(node):
-            for neighbor in adj[node]:
-                if not visited[neighbor]:
-                    visited[neighbor] = True
-                    path_visited[neighbor] = True
-                    if dfs(neighbor):
-                        # safenodes[neighbor] = 0
-                        return True
-                    path_visited[neighbor] = False
-                elif visited[neighbor] and path_visited[neighbor]:
-                    # safenodes[neighbor] = 0
-                    return True
-
-            safenodes[node] = 1
-            path_visited[node] = False
-            return False
-
-        res = []       
-        for start in range(n):
-            if not visited[start]:
-                dfs(start)
-
+                adj[node].append(i)
+                indegrees[i] += 1
+        
+        Q = deque()
         for i in range(n):
-            if safenodes[i]:
-                res.append(i)
+            if indegrees[i]==0:
+                Q.append(i)
 
-        return res
-                
+        res = []
+        while Q:
+            current = Q.popleft()
+            res.append(current)
+
+            for neighbor in adj[current]:
+                indegrees[neighbor] -= 1
+                if indegrees[neighbor] == 0:
+                    Q.append(neighbor)
+
+        return sorted(res)
+
+
+
+        
