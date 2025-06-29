b965cb35aaab28a980e301634fbe692d2f315c35
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-29 17:32:32 -0500

    Time: 427 ms (5.12%), Space: 23.3 MB (7.16%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -3,6 +3,7 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
         if grid[n-1][n-1] !=0: return -1
+
         adj = defaultdict(list)
         distance = defaultdict(int)
 
@@ -12,27 +13,27 @@ class Solution:
         for i in range(n):
             for j in range(n):
                 if grid[i][j] ==0:
-                    distance[(i,j)] = float('inf')
+                    distance[i*n+j] = float('inf')
                     for dx, dy in dirs:
                         x,y = i+dx, j+dy
                         if 0<=x<n and 0<=y<n:
                             if grid[x][y]==0 : 
-                                adj[(i,j)].append((x,y))
+                                adj[i*n+j].append(x*n+y)
         Q = deque()
-        Q.append([0,0,1])
+        Q.append([0,1])
         while Q:
-            x,y, d = Q.popleft()
-            if d<distance[(x,y)] : 
-                distance[(x,y)]  = d
+            node, d = Q.popleft()
+            if d<distance[node] : 
+                distance[node]  = d
 
-                for n_x, n_y in adj[(x,y)]:
+                for ngh in adj[node]:
                     
-                    Q.append([n_x, n_y, d+1])
+                    Q.append([ngh, d+1])
 
 
-        if distance[(n-1, n-1)] == float('inf'): return -1
+        if distance[n**2-1] == float('inf'): return -1
 
-        return distance[(n-1, n-1)]
+        return distance[n**2-1]
                 
             
 
