e995f64e5e41c4b2e30346be8de5f4a79b4fb13d
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-30 12:29:57 -0500

    Time: 24 ms (25.59%), Space: 24.9 MB (96.05%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
@@ -15,15 +15,15 @@ class Solution:
         pq = [(0,0)]
         while pq:
             time, node = heapq.heappop(pq)
-            # if time > times[node]: 
-            #     continue
+            if time > times[node]: 
+                continue
             for ngh, time_ in adj[node]:
                 if time+time_ < times[ngh]:
                     heapq.heappush(pq,(time+time_, ngh))
                     times[ngh] = time+time_
                     counts[ngh] = counts[node]
                 elif time+time_ == times[ngh]:
-                    counts[ngh] += counts[node]
+                    counts[ngh] +=counts[node]
         
         return counts[n-1]%MOD
         
