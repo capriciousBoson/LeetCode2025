b824c079adaf89ba0a30b94509f74d78e87b277c
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-29 21:46:16 -0500

    Time: 377 ms (71.83%), Space: 19.6 MB (92.75%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        time_taken = [float('inf') for _ in range(n+1)]
+        adj = defaultdict(list)
+
+        for u,v,w in times:
+            adj[u].append((v,w))
+        
+        print(f"adjacency list : {adj}")
+        pq = [(0,k)]
+        time_taken[k] = 0
+
+        while pq:
+            time, node = heapq.heappop(pq)
+
+            for ngh, time_ in adj[node]:
+                if time+time_ < time_taken[ngh]:
+                    heapq.heappush(pq, (time+time_, ngh))
+                    time_taken[ngh] = time+time_
+
+        print(f"timetaken : {time_taken}")
+        res = max(time_taken[1:])
+        if res == float('inf'): return -1
+        return res        
