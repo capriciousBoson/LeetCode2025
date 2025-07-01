ca0adb3ea34493f1fc541702a5a9e74bf96605d0
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-01 11:46:52 -0500

    Time: 55 ms (22.33%), Space: 32.7 MB (48.29%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
@@ -4,19 +4,29 @@ class Solution:
         if num_edges<n-1: return -1
         
         parent = [i for i in range(n)]
+        rank = [0 for  _ in range(n)]
 
         def find(x):
             if parent[x] !=x:
                 parent[x] = find(parent[x])
             return parent[x]
-        def union(a,b):
+
+        def unionbyrank(a,b):
             root_a = find(a)
             root_b = find(b)
+            if root_a == root_b: return
 
-            if root_a != root_b:
+            if rank[root_a] < rank[root_b]:
+                parent[root_a] = root_b
+            elif rank[root_a] > rank[root_b]:
+                parent[root_b] = parent[root_a]
+            else:
+
                 parent[root_b] = root_a
+                rank[root_a] += 1
+
         for u,v in connections:
-            union(u,v)
+            unionbyrank(u,v)
         
         connected_components = set()
         for i in range(n):
