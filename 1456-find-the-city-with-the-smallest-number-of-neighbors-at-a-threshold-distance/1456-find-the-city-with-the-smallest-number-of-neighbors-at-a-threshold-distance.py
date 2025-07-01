4c8c1fed9cae7fce9786c4a01822b63bde8a5327
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-06-30 19:52:44 -0500

    Time: 827 ms (6.23%), Space: 18.9 MB (82.37%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,15 +1,16 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         #using bellman ford algo.
-        edges_  = []
-        for u,v,w in edges:
-            edges_.append([u,v,w])
-            edges_.append([v,u,w])
+        # edges_  = []
+        # for u,v,w in edges:
+        #     edges_.append([u,v,w])
+        #     edges_.append([v,u,w])
 
         distances = [[float('inf') for _ in range(n)] for __ in range(n)]
        
-        for u, v, w in edges_:
+        for u, v, w in edges:
             distances[u][v] = w
+            distances[v][u] = w
 
         for i in range(n):
             distances[i][i] = 0
