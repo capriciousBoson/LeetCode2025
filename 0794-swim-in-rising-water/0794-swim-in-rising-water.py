b5df28b074078b6893133993b92fc29383d0662a
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-02 10:27:33 -0500

    Time: 36 ms (38.59%), Space: 18.4 MB (72.78%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -0,0 +1,31 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+
+        Q = []
+        Q.append((grid[0][0],0,0))
+        res = -float('inf')
+        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+        visited = [[False for _ in range(n)] for __ in range(n)]
+        path = []
+        reached = False
+
+        while Q:
+            elevation, i,j = heapq.heappop(Q)
+            res = max(res, elevation)
+            visited[i][j] = True
+            path.append(grid[i][j])
+            if i==n-1 and j==n-1: break
+
+            for dx, dy in dirs:
+                x,y = i+dx, j+dy
+                if 0<=x<n and 0<=y<n and not visited[x][y]:
+                    heapq.heappush(Q,(grid[x][y], x, y))
+
+        print(f"visited: {path}")
+
+        return res
+
+
+
+        
