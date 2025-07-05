3081b2fdf360cdd4b7f5dacf62fc61196eee3663
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-05 18:15:57 -0500

    Time: 83 ms (5.05%), Space: 20.3 MB (10.18%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        ns = len(s)
+        ng = len(g)
+        if ns==0 or ng==0: return 0
+
+        
+
+        # max_cookie = max(s)
+        g = sorted(g, reverse=True)
+        s = sorted(s, reverse=True)
+
+        g_p = 0
+        s_p = 0
+        
+
+        res = 0 
+        while g_p < ng and s_p < ns:
+            print(f"current greed = {g[g_p]} , cookie size : {s[s_p]}")
+            if s[s_p] >= g[g_p]:
+                res += 1
+                s_p += 1
+                g_p += 1
+                print(f"updated satisfied to :  {res}")
+            elif s[s_p] < g[g_p]:
+                g_p +=1
+        
+        return res
+            
+            
+
+        return res       
