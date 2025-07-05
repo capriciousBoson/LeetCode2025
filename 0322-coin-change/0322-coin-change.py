965454fc05e7b737d6c1ecbbbfa73a36e0f3b064
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-05 18:52:34 -0500

    Time: 1170 ms (16.18%), Space: 45.5 MB (9.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,27 +1,33 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort(reverse = True)
+        # coins.sort(reverse = True)
         s = len(coins)
 
         memo = {}
 
-        def dfs(i, money):
+        def dfs( money):
         
             if money == amount:
                 return 0
-            if money > amount or i==s:
+            if money > amount:
                 return float('inf')
 
-            if (i, money) in memo: return memo[(i,money)]
-            take_and_keep = 1 + dfs(i, money + coins[i])
-            take_and_move = 1 + dfs(i+1, money + coins[i])
-            not_take = dfs(i+1, money)
+            if money in memo: 
+                return memo[money]
+            
+            res = float('inf')
+            # take_and_keep = 1 + dfs(i, money + coins[i])
+            # take_and_move = 1 + dfs(i+1, money + coins[i])
+            # not_take = dfs(i+1, money)
 
-            memo[(i,money)] = min(take_and_keep,take_and_move, not_take )
+            for coin in coins:
+                res =min(res, 1+ dfs(money+coin))
 
-            return  memo[(i,money)]
+            memo[money] =res
+
+            return  memo[money]
         
-        n = dfs(0,0)
+        n = dfs(0)
         if n==float('inf'): return -1
         return n
 
