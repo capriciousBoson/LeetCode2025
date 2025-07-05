c3d971858d4ba7c836a5fdb26e78a1ef7121b2e1
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-05 18:21:27 -0500

    Time: 82 ms (5.05%), Space: 20.2 MB (11.94%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -6,6 +6,8 @@ class Solution:
 
         g = sorted(g, reverse=True)
         s = sorted(s, reverse=True)
+        # g.sort()
+        # s.sort()
 
         g_p = 0
         s_p = 0
@@ -13,7 +15,7 @@ class Solution:
 
         res = 0 
         while g_p < ng and s_p < ns:
-            # print(f"current greed = {g[g_p]} , cookie size : {s[s_p]}")
+            print(f"current greed = {g[g_p]} , cookie size : {s[s_p]}")
             if s[s_p] >= g[g_p]:
                 res += 1
                 s_p += 1
