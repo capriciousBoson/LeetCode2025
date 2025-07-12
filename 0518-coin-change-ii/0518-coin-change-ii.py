8088f58771497f28caed5df1c870952f8a0b1e24
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-12 18:02:14 -0500

    Time: 459 ms (29.15%), Space: 49 MB (39.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,23 +1,35 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo = {}
         n = len(coins)
-        
-        def dfs(x, i):
+        dp = [[0 for _ in range(n+1)] for __ in range(amount  +1)]
+        for amt in range(amount+1):
+            dp[amt][n] = 0
+        dp[0][n]=1
+
+
+        for amt  in range(amount+1):
+            for i in range(n-1, -1, -1):
+                dp[amt][i] = dp[amt][i+1]
+                if amt-coins[i] >=0:
+                    dp[amt][i] += dp[amt-coins[i]][i]
+        return dp[amount][0]
+        # memo = {}
+
+        # def dfs(x, i):
             
-            if x==amount:
-                return  1
+        #     if x==amount:
+        #         return  1
 
-            elif x > amount:
-                return 0
+        #     elif x > amount:
+        #         return 0
 
-            if i==n : return 0
+        #     if i==n : return 0
 
-            if (x,i) not in memo:
-                memo[(x,i)] = dfs(x+coins[i], i ) + dfs(x, i+1)
-            return memo[(x,i)]
+        #     if (x,i) not in memo:
+        #         memo[(x,i)] = dfs(x+coins[i], i ) + dfs(x, i+1)
+        #     return memo[(x,i)]
 
 
-        return dfs(0, 0)
-
+        # return dfs(0, 0)
+ 
         
