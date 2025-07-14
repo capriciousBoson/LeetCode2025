b618fe0ba89b2049c2ded15533fd35f1f758a970
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-14 12:56:19 -0500

    Time: 507 ms (44.64%), Space: 43.6 MB (25.91%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,16 +1,34 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
 
-        memo = {}
-        def dfs(i1, i2):
-            if i1 < 0 or i2<0:
-                return 0
-            if (i1, i2) not in memo:
-                if text1[i1] == text2[i2]:
-                    memo[(i1, i2)] = 1 + dfs(i1-1, i2-1)
-                else:
-                    memo[(i1, i2)] = max(dfs(i1-1, i2), dfs(i1, i2-1))
-            return memo[(i1, i2)]
+        m,n = len(text1), len(text2)
+        dp = [[-1 for _ in range(n+1)] for j in range(m+1)]
 
-        return dfs(len(text1)-1, len(text2)-1)
+        for i in range(m+1):
+            dp[i][0] = 0
+        for j in range(n+1):
+            dp[0][j] = 0
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[m][n]
+
+
+
+        # memo = {}
+        # def dfs(i1, i2):
+        #     if i1 < 0 or i2<0:
+        #         return 0
+        #     if (i1, i2) not in memo:
+        #         if text1[i1] == text2[i2]:
+        #             memo[(i1, i2)] = 1 + dfs(i1-1, i2-1)
+        #         else:
+        #             memo[(i1, i2)] = max(dfs(i1-1, i2), dfs(i1, i2-1))
+        #     return memo[(i1, i2)]
+
+        # return dfs(len(text1)-1, len(text2)-1)
         
