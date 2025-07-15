9b7e3bbeab86c059f6b47433fa6bab4acdfaf779
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-14 20:10:36 -0500

    Time: 358 ms (83.21%), Space: 19.5 MB (56.63%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -2,19 +2,26 @@ class Solution:
     def minInsertions(self, s: str) -> int:
 
         n = len(s)
-        dp = [[0 for _ in range(n+1)] for __ in range(n+1)]
+        dp = [[0 for _ in range(n)] for __ in range(n)]
 
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                if s[i-1] == s[(n-1)-(j-1)]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+        for i in range(n):
+            dp[i][i] = 0
+
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                # print(f"\n i,j : {i,j} ::::: s[{i}] = {s[i]}  s[{j}] = {s[j]}")
+                if s[i]==s[j]:
+                    # print(f"same characters---- \ndp[i+1][j-1] : {dp[i+1][j-1]}")
+                    dp[i][j] = dp[i+1][j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-        return n-dp[n][n]
+                    # print(f"different characters--- \ndp[{i+1}][{j}]={dp[i+1][j]}, dp[{i}][{j-1}]={dp[i][j-1]}")
+                    dp[i][j] = 1 + min(
+                                            dp[i+1][j],
+                                            dp[i][j-1]
         
-
-
+                                        )
+        # for row in dp: print([str(n)+"   " if n==0 else str(n)+"." for n in row])
+        return dp[0][n-1]
 
 
         # MEMOIZATION BASED SOLUTION --------------
