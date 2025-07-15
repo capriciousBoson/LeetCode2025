e6cb20d42829a5bb7c022723446c5e38ce2859d3
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-14 20:07:49 -0500

    Time: 767 ms (20.26%), Space: 19.7 MB (46.02%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,19 +1,37 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        
-        n = len(s)
-        memo = {}
-        def util(start, end):
-            if start >= end :
-                return 0
-            if (start,end) not in memo :
 
-                if s[start]==s[end]:
-                    memo[(start, end)] = util(start+1, end-1)
+        n = len(s)
+        dp = [[0 for _ in range(n+1)] for __ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if s[i-1] == s[(n-1)-(j-1)]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
                 else:
-                    memo[(start, end)] = 1 + min(
-                                                    util(start+1, end), 
-                                                    util(start, end-1)
-                                                    )
-            return memo[(start, end)] 
-        return util(0,n-1)
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return n-dp[n][n]
+        
+
+
+
+
+        # MEMOIZATION BASED SOLUTION --------------
+
+        # n = len(s)
+        # memo = {}
+        # def util(start, end):
+        #     if start >= end :
+        #         return 0
+        #     if (start,end) not in memo :
+
+        #         if s[start]==s[end]:
+        #             memo[(start, end)] = util(start+1, end-1)
+        #         else:
+        #             memo[(start, end)] = 1 + min(
+        #                                             util(start+1, end), 
+        #                                             util(start, end-1)
+        #                                             )
+        #     return memo[(start, end)] 
+        # return util(0,n-1)
