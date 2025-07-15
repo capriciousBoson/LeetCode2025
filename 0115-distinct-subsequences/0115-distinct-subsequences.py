c9377256cedfddb4720021542f9b602bb746240c
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-15 18:42:37 -0500

    Time: 463 ms (45.18%), Space: 74.7 MB (37.94%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,20 +1,38 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
+
         m,n = len(s), len(t)
-        memo = {}
 
-        def dfs(i, j):
-            if j < 0:
-                return 1
-            if i<0 :
-                return 0
+        dp = [[0 for _ in range(n+1)] for __ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = 1
 
-            if (i,j) not in memo:
-                if s[i]==t[j]:
-                    memo[(i,j)] =  dfs(i-1, j-1) + dfs(i-1, j)
+        for i in  range(1, m+1):
+            for j in range(1,n+1):
+                
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
                 else:
-                     memo[(i,j)] =  dfs(i-1, j)
-                     
-            return memo[(i,j)]
+                    dp[i][j] = dp[i-1][j]
 
-        return dfs(m-1, n-1)
+        # for row in dp: print(row)
+        return dp[m][n]
+
+
+        # m,n = len(s), len(t)
+        # memo = {}
+        # def dfs(i, j):
+        #     if j < 0:
+        #         return 1
+        #     if i<0 :
+        #         return 0
+
+        #     if (i,j) not in memo:
+        #         if s[i]==t[j]:
+        #             memo[(i,j)] =  dfs(i-1, j-1) + dfs(i-1, j)
+        #         else:
+        #              memo[(i,j)] =  dfs(i-1, j)
+
+        #     return memo[(i,j)]
+
+        # return dfs(m-1, n-1)
