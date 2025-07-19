755895b5533037b71cf82e537d1451f9fcac2287
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-19 14:06:02 -0500

    Time: 38 ms (5.42%), Space: 27.8 MB (5.02%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,34 +1,71 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
+        # dp = [[0,0] for __ in range(n+1)]
+
+        # for 
+
+
+
+        
+        ########################################
+        ####         MEMOIZATION I          ####
+        ########################################
         n = len(prices)
-
         memo = {}
-
-        def dfs(i, j):
-            # print(f"\n buy-{i} sell-{j} ---------------")
-            if i>=n or j>=n: 
+        def dfs(i, can_buy):
+            if i>=n:
                 return 0
-            if i>= j: 
-                return dfs(i, j+1)
-            if prices[i] >= prices[j]:
-                return dfs(i+1, j)
-            if (i,j) not in memo:
-                buy_and_sell = prices[j] - prices[i] + dfs(j, j+1)
-                buy_dont_sell = dfs(i, j+1)
-                dont_buy = dfs(i+1, j)
+            if (i, can_buy) not in memo: 
+                if can_buy:
+                    memo[(i, can_buy)] = max( 
+                                                -prices[i] + dfs(i+1, False),
+                                                dfs(i+1, True)
+                                                )
 
-                # print(f"buy and sell at {prices[i],prices[j]}  : {buy_and_sell}")
-                # print(f"buy dont sell at {prices[i],prices[j]}   : {buy_dont_sell}")
-                # print(f"dont buy at {prices[i],prices[j]}  : {dont_buy}")
+                if not can_buy:
+                    memo[(i, can_buy)] = max(   
+                                                prices[i] + dfs(i+1, True),
+                                                dfs(i+1, False)
+                                                )
+                            
+                            
+            return memo[(i, can_buy)]
 
-                memo[(i,j)] = max(buy_and_sell, buy_dont_sell, dont_buy)
+        return dfs(0, True)
 
-            # print(f"max profit for {prices[i],prices[j]} = {memo[(i,j)]}")
-            return memo[(i,j)]
 
-            
+        ########################################
+        ####         MEMOIZATION II         ####
+        ########################################
 
-        return dfs(0,0)
+        # n = len(prices)
+
+        # memo = {}
+
+        # def dfs(i, j):
+        #     # print(f"\n buy-{i} sell-{j} ---------------")
+        #     if i>=n or j>=n: 
+        #         return 0
+        #     if i>= j: 
+        #         return dfs(i, j+1)
+        #     if prices[i] >= prices[j]:
+        #         return dfs(i+1, j)
+        #     if (i,j) not in memo:
+        #         buy_and_sell = prices[j] - prices[i] + dfs(j, j+1)
+        #         buy_dont_sell = dfs(i, j+1)
+        #         dont_buy = dfs(i+1, j)
+
+        #         # print(f"buy and sell at {prices[i],prices[j]}  : {buy_and_sell}")
+        #         # print(f"buy dont sell at {prices[i],prices[j]}   : {buy_dont_sell}")
+        #         # print(f"dont buy at {prices[i],prices[j]}  : {dont_buy}")
+
+        #         memo[(i,j)] = max(buy_and_sell, buy_dont_sell, dont_buy)
+
+        #     # print(f"max profit for {prices[i],prices[j]} = {memo[(i,j)]}")
+        #     return memo[(i,j)]
+
+        # return dfs(0,0)
 
 
 
