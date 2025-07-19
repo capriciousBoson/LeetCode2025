16d19b3513bfcd794bee38337043b286f94ffa60
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-19 13:04:39 -0500

    Time: 358 ms (5.42%), Space: 55.9 MB (5.02%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+
+        memo = {}
+
+        def dfs(i, j):
+            # print(f"\n buy-{i} sell-{j} ---------------")
+            if i>=n or j>=n: 
+                return 0
+            if i>= j: 
+                return dfs(i, j+1)
+            if prices[i] >= prices[j]:
+                return dfs(i+1, j)
+            if (i,j) not in memo:
+                buy_and_sell = prices[j] - prices[i] + dfs(j, j+1)
+                buy_dont_sell = dfs(i, j+1)
+                dont_buy = dfs(i+1, j)
+
+                # print(f"buy and sell at {prices[i],prices[j]}  : {buy_and_sell}")
+                # print(f"buy dont sell at {prices[i],prices[j]}   : {buy_dont_sell}")
+                # print(f"dont buy at {prices[i],prices[j]}  : {dont_buy}")
+
+                memo[(i,j)] = max(buy_and_sell, buy_dont_sell, dont_buy)
+
+            # print(f"max profit for {prices[i],prices[j]} = {memo[(i,j)]}")
+            return memo[(i,j)]
+
+            
+
+        return dfs(0,0)
+
+
+
+
+
