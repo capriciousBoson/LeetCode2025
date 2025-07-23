797fcfe7d6c96d1cdb7143fd42ad910968fad662
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-23 13:47:25 -0500

    Time: 855 ms (27.09%), Space: 45.2 MB (27.35%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,21 +1,33 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
+        dp = [[0 for _ in range(5)] for __  in range(n+1)]
 
-        memo = {}
+        for i in range(n-1, -1, -1):
+            for c in range(4):
+                if c in (0,2):
+                    dp[i][c] = max(-prices[i]+dp[i+1][c+1], dp[i+1][c])
+                if c in (1,3):
+                    dp[i][c] = max(+prices[i]+dp[i+1][c+1], dp[i+1][c])
 
-        def dfs(i, c):
-            if i >= n or c>=4:
-                return 0
-            if (i,c) not in memo:
+
+        return dp[0][0]
+
+
+        # memo = {}
+
+        # def dfs(i, c):
+        #     if i >= n or c>=4:
+        #         return 0
+        #     if (i,c) not in memo:
                 
 
-                if c in (0,2):
-                    memo[(i,c)] = max(-prices[i]+dfs(i+1, c+1), dfs(i+1,c))
-                if c in (1,3):
-                    memo[(i,c)] =  max(prices[i] + dfs(i+1, c+1), dfs(i+1,c))
-            return memo[(i,c)]
-        return dfs(0,0)
+        #         if c in (0,2):
+        #             memo[(i,c)] = max(-prices[i]+dfs(i+1, c+1), dfs(i+1,c))
+        #         if c in (1,3):
+        #             memo[(i,c)] =  max(prices[i] + dfs(i+1, c+1), dfs(i+1,c))
+        #     return memo[(i,c)]
+        # return dfs(0,0)
 
 
 
