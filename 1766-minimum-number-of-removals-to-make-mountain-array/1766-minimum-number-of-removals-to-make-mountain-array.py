7c6d562a0c586134ed7acd2782d2c56c5c6abb6e
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-02 18:07:16 -0500

    Time: 1879 ms (23.22%), Space: 17.9 MB (50.36%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.py
@@ -8,14 +8,17 @@ class Solution:
             for prev in range(i):
                 if nums[i]>nums[prev]:
                     dp1[i] = max(dp1[i], 1+dp1[prev])
-                if nums[-i-1] > nums[-prev-1]:
-                    dp2[i] = max(dp2[i], 1+dp2[prev])
+
+        for j in range(n-1, -1, -1):
+            for prev in range(n-1, j, -1):
+                if nums[j]>nums[prev]:
+                    dp2[j] = max(dp2[j], 1+dp2[prev])
 
 
         lb  = 0
         for i in range(n):
-            if dp1[i]>1 and dp2[-i-1] >1:
-                lb = max(lb, dp1[i] + dp2[-i-1]-1)
+            if dp1[i]>1 and dp2[i] >1:
+                lb = max(lb, dp1[i] + dp2[i]-1)
         
         
         return n-lb
